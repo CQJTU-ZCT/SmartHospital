279dc70dd0eac6e6a6b6710d6cee9e33fe690a7c
zjhfyq <799243917@qq.com>
2017-12-11 15:13:55 +0800

    修改pom文件结构，添加ApiGateWay模块

---
+++ api-gateway/src/main/java/com/cqjtu/ApiGateWayApplication.java
@@ -0,0 +1,22 @@
+package com.cqjtu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author zjhfyq
+ * @Desc
+ * @date 2017/12/11.
+ */
+@EnableAutoConfiguration
+@EnableZuulProxy
+@EnableEurekaClient
+public class ApiGateWayApplication {
+    public static void main(String [] args){
+        SpringApplication.run(ApiGateWayApplication.class,args);
+    }
+}
