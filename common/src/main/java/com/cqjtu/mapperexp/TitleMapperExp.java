02c28efb7bc7b026479f8e65c69852589be51140
zjhfyq <799243917@qq.com>
2018-01-26 20:03:42 +0800

    完成职称信息的操作逻辑

---
+++ common/src/main/java/com/cqjtu/mapperexp/TitleMapperExp.java
@@ -0,0 +1,37 @@
+package com.cqjtu.mapperexp;
+
+import com.cqjtu.model.Title;
+
+import java.util.List;
+
+/**
+ * @author zjhfyq
+ * @Desc
+ * @date 2018/1/26.
+ */
+public interface TitleMapperExp {
+
+    /**
+     * 添加职称信息
+     * @param title
+     * @return
+     */
+    int addTitle(Title title);
+
+
+    /**
+     * 修改职称信息
+     * @param title
+     * @return
+     */
+    int updateTitle(Title title);
+
+
+    /**
+     * 查询职称
+     * @param name
+     * @return
+     */
+    List<Title> queryTitle(String name);
+
+}
