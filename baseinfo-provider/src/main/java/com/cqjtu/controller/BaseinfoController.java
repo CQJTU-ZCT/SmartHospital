d2e9fec744bb2e7b4b577e069f2e71fbc1163e83
zjhfyq <799243917@qq.com>
2018-02-06 16:05:01 +0800

    添加基础信息模块  并且完成nation  sex的基础信息获取控制器

---
+++ baseinfo-provider/src/main/java/com/cqjtu/controller/BaseinfoController.java
@@ -0,0 +1,9 @@
+package com.cqjtu.controller;
+
+/**
+ * @author zjhfyq
+ * @Desc
+ * @date 2018/2/6.
+ */
+public class BaseinfoController {
+}
---
+++ baseinfo-provider/src/main/java/com/cqjtu/service/NationServiceImpl.java
@@ -0,0 +1,9 @@
+package com.cqjtu.service;
+
+/**
+ * @author zjhfyq
+ * @Desc
+ * @date 2018/2/6.
+ */
+public class NationServiceImpl {
+}
